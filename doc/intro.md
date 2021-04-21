dc7e7462d62fb102c26ccb3e18477ed1add709af
Eric Ihli <eihli@owoga.com>
2021-04-21 14:38:30 -0500

    Initial commit after clj-new.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to syllabify
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/owoga/syllabify_test.clj
@@ -0,0 +1,7 @@
+(ns com.owoga.syllabify-test
+  (:require [clojure.test :refer :all]
+            [com.owoga.syllabify :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
